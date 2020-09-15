eacd91ac6465a0b83059e4f36dfe2fe9b50f1d57
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2020-09-15 16:07:40 +0200

    create order by struct

---
+++ order-by.go
@@ -0,0 +1,25 @@
+package visitypes
+
+type Order string
+
+const (
+	OrderAsc  = "ASC"
+	OrderDesc = "DESC"
+)
+
+type OrderBy struct {
+	field string
+	order Order
+}
+
+func NewOrderBy(field string, order Order) *OrderBy {
+	return &OrderBy{field: field, order: order}
+}
+
+func (ob *OrderBy) GetField() string {
+	return ob.field
+}
+
+func (ob *OrderBy) GetOrder() string {
+	return string(ob.order)
+}
